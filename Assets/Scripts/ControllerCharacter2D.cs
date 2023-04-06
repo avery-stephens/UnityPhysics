4d2baae41f8f9638987054ee40bd0fd014845a3a
avery-stephens <astephens@student.neumont.edu>
2023-04-06 11:18:17 -0600

    added sprites and tilemaps

---
+++ Assets/Scripts/ControllerCharacter2D.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Search;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ControllerCharacter2D : MonoBehaviour
+{
+	[SerializeField] Animator animator;
+	[SerializeField] SpriteRenderer spriteRenderer;
+	[SerializeField] float speed;
+	[SerializeField] float turnRate;
+	[SerializeField] float jumpHeight;
+	[SerializeField] float doubleJumpHeight;
+	[SerializeField] float hitForce;
+	[SerializeField, Range(1, 5)] float fallRateMultiplier;
+	[SerializeField, Range(1, 5)] float lowJumpRateMultiplier;
+	[Header("Ground")]
+	[SerializeField] Transform groundTransform;
+	[SerializeField] LayerMask groundLayerMask;
+
+	Rigidbody2D rb;
+	Vector2 velocity = Vector2.zero;
+	bool faceRight = true;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		bool onGround = Physics2D.OverlapCircle(groundTransform.position, 0.02f, groundLayerMask) != null;
+
+		// get direction input
+		Vector2 direction = Vector2.zero;
+		direction.x = Input.GetAxis("Horizontal");
+
+		velocity.x = direction.x * speed;
+		// set velocity
+		if (onGround)
+		{
+			if(velocity.y < 0) velocity.y = 0;
+			if (Input.GetButtonDown("Jump"))
+			{
+				velocity.y += Mathf.Sqrt(jumpHeight * -2 * Physics.gravity.y);
+				StartCoroutine(DoubleJump());
+				animator.SetTrigger("Jump");
+			}
+		}
+
+		velocity.y += Physics.gravity.y * Time.deltaTime;
+
+		//adjust gravity for jump
+		float gravityMultiplier = 1;
+		if (!onGround && velocity.y < 0) gravityMultiplier = fallRateMultiplier;
+		if (!onGround && velocity.y > 0 && !Input.GetButton("Jump")) gravityMultiplier = lowJumpRateMultiplier;
+
+		velocity.y += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+
+		//move character
+		rb.velocity = velocity;
+
+		// flip character to face direction of movement
+		if (velocity.x > 0 && !faceRight) Flip();
+		if (velocity.x < 0 && faceRight) Flip();
+
+		//update animator
+		animator.SetFloat("Speed", Mathf.Abs(velocity.x));
+	}
+
+	IEnumerator DoubleJump()
+	{
+		// wait a little after the jump to allow a double jump
+		yield return new WaitForSeconds(0.01f);
+		// allow a double jump while moving up
+		while (velocity.y > 0)
+		{
+			// if "jump" pressed add jump velocity
+			if (Input.GetButtonDown("Jump"))
+			{
+				velocity.y += Mathf.Sqrt(doubleJumpHeight * -2 * Physics.gravity.y);
+				break;
+			}
+			yield return null;
+		}
+	}
+
+	private void Flip()
+	{
+		faceRight = !faceRight;
+		spriteRenderer.flipX = !faceRight;
+	}
+
+}
